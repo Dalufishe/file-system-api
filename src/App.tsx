6921f733b7e29441dc4c45e189ab804e71a7e674
Dalufishe <fishawe87@gmail.com>
2024-06-26 22:58:06 +0800

    add three

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import FileSystem from "./components/FileSystem/FileSystem";
+import ThreeApplication from "./three/yao.js";
 
 function App() {
+  useEffect(() => {
+    ThreeApplication();
+  });
+
   return (
     <div className="relative">
       <FileSystem />
+      <canvas className="webgl"></canvas>
     </div>
   );
 }
